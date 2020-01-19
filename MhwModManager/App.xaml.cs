aacdf9d59903c31ce825093734984f0611dec72f
OxyTom <oxytom8@gmail.com>
2020-01-19 09:58:17 +0100

    Added updater

--- MhwModManager/App.xaml.cs
+++ MhwModManager/App.xaml.cs
@@ -13,5 +13,18 @@ namespace MhwModManager
     /// </summary>
     public partial class App : Application
     {
+        public async static void Updater()
+        {
+            /* Credits to WildGoat07 : https://github.com/WildGoat07 */
+            var github = new Octokit.GitHubClient(new Octokit.ProductHeaderValue("MhwModManager"));
+            var lastRelease = await github.Repository.Release.GetLatest(234864718);
+            var current = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+            if (new Version(lastRelease.TagName) > current)
+            {
+                var result = MessageBox.Show("Une nouvelle version est disponible, voulez-vous la télécharger ?", "MHW Mod Manager", MessageBoxButton.YesNo, MessageBoxImage.Information);
+                if (result == MessageBoxResult.Yes)
+                    System.Diagnostics.Process.Start("https://github.com/oxypomme/MhwModManager/releases");
+            }
+        }
     }
 }
